85b50ce18d88e2cc593f66ad919392dc3666a4a8
A-S-M-Sohag-Abdullah <sohagadullah2021@gmail.com>
2025-06-19 23:01:25 +0600

    Add frontend as part of main repo

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("https://testapp-of9p.onrender.com/api/test")
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>React Frontend</h1>
+      <p>API Response: {data}</p>
+    </div>
+  );
+}
+
+export default App;
